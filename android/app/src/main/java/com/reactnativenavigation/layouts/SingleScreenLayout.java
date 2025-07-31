e67e6c872acec6ba85af6fa45ca374e5da2072ae
Anny <anny86023@gmail.com>
2025-07-31 09:29:10 +0800

    add import

--- android/app/src/main/java/com/reactnativenavigation/layouts/SingleScreenLayout.java
+++ android/app/src/main/java/com/reactnativenavigation/layouts/SingleScreenLayout.java
@@ -1,5 +1,7 @@
 package com.reactnativenavigation.layouts;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
